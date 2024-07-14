1e520f9639505a350bb0258e7e2bf73d70335b7f
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-07-14 17:50:33 +0530

    adding routes and components...

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,72 @@
+import { Navigate, Route, Routes, useLocation } from "react-router-dom"
+import Toaster from "sonner"
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
+import TaskDetails from "./pages/TaskDetails"
+import Tasks from "./pages/Tasks"
+import Trash from "./pages/Trash"
+import Users from "./pages/Users"
+
+
+
+
+function Layout(){
+  const user ="";
+
+  const location = useLocation()
+
+  return  user ? (
+      <div className="w-full h-screen flex flex-col md:flex-row">
+        <div>
+          
+        </div>
+      </div>
+  ):(
+    <Navigate to="/log-in" state={{from : location}} replace/>  //The state prop is used to pass additional state information to the destination route(here login page).
+                                    //location typically refers to the current location object,This is useful for keeping track of where the user was before being redirected.
+                                    //The replace prop, when set to true, changes the navigation behavior. This means the user won't be able to navigate back to the previous page using the back button after being redirected to /log-in.
+  )
+
+
+}
+
+
+
+
+
+
+
 
 
 function App() {
-  
+
   return (
-    <div>
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
-       
-    </div>
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+
+      <Routes>
+
+        <Route element={<Layout />}>
+
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/task" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/team" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+
+        </Route>
+
+             <Route path="/log-in" element={<Login />} />
+
+
+      </Routes>
+
+      <Toaster richColors/>
+
+    </main>
   )
 }
 
--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+    
+   login
+
+    </div>
   )
 }
 
