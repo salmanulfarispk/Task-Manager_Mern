0469f03a8f4a86d3e46a7e814737c6f7b70ce98f
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-07-16 06:12:25 +0530

    mobilenav completes..

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,51 +5,113 @@ import TaskDetails from "./pages/TaskDetails"
 import Tasks from "./pages/Tasks"
 import Trash from "./pages/Trash"
 import Users from "./pages/Users"
-import {Toaster} from "sonner"
-import { useSelector } from "react-redux"
+import { Toaster } from "sonner"
+import { useDispatch, useSelector } from "react-redux"
 import Sidebar from "./components/Sidebar"
 import Navbar from "./components/Navbar"
+import { useRef } from "react"
+import { setOpenSidebar } from "./redux/slices/authSlice"
+import { Transition } from "@headlessui/react"
+import clsx from "clsx"
+import { IoClose } from "react-icons/io5"
 
 
 
-
-function Layout(){
-  const {user} = useSelector((state)=> state.auth) ;
+function Layout() {
+  const { user } = useSelector((state) => state.auth);
 
   const location = useLocation()
 
-  return  user ? (
-      <div className="w-full h-screen flex flex-col md:flex-row">
-        <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
-         {/**Sidebar */}
-             <Sidebar/> 
-        </div>
-
-           {/**here  Mobile  Sidebar component*/}
-
-            
-
-          <div className="flex-1 overflow-y-auto">
-               {/**Navbar */}
-               <Navbar/>
-
-               <div className="p-4  2xl:px-10">
-                   <Outlet/> {/**here inside outlet ,all other components works except sidebars and navbars */}
-               </div>
-          </div>
-
-
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/**Sidebar */}
+        <Sidebar />
       </div>
-  ):(
-    <Navigate to="/log-in" state={{from : location}} replace/>  //The state prop is used to pass additional state information to the destination route(here login page).
-                                    //location typically refers to the current location object,This is useful for keeping track of where the user was before being redirected.
-                                    //The replace prop, when set to true, changes the navigation behavior. This means the user won't be able to navigate back to the previous page using the back button after being redirected to /log-in.
+
+      {/**here  Mobile  Sidebar component*/}
+          <MobileNavbar/>
+
+
+      <div className="flex-1 overflow-y-auto">
+        {/**Navbar */}
+        <Navbar />
+
+        <div className="p-4  2xl:px-10">
+          <Outlet /> {/**here inside outlet ,all other components works except sidebars and navbars */}
+        </div>
+      </div>
+
+
+    </div>
+  ) : (
+    <Navigate to="/log-in" state={{ from: location }} replace />  //The state prop is used to pass additional state information to the destination route(here login page).
+    //location typically refers to the current location object,This is useful for keeping track of where the user was before being redirected.
+    //The replace prop, when set to true, changes the navigation behavior. This means the user won't be able to navigate back to the previous page using the back button after being redirected to /log-in.
   )
 
 
 }
 
 
+
+const MobileNavbar = () => {
+
+  const { isSidebarOpen } = useSelector((state) => state.auth)
+  const mobileMenuRef = useRef(null)
+  const dispatch = useDispatch()
+
+
+  const closeSidebar = () => {
+    dispatch(setOpenSidebar(false))
+  }
+
+
+  return (
+    <>
+
+      <Transition
+        show={isSidebarOpen}
+        enter='transition-opacity duration-700'
+        enterFrom='opacity-x-10'
+        enterTo='opacity-x-100'
+        leave='transition-opacity duration-700'
+        leaveFrom='opacity-x-100'
+        leaveTo='opacity-x-0'
+      >
+
+        {(ref)=> (
+          <div ref={(node)=> (mobileMenuRef.current = node)}
+          className={clsx(
+            "md:hidden w-full h-full bg-black/40 transition-all duration-700 transform",
+             isSidebarOpen ? "translate-x-0" : "translate-x-full"
+          )}
+            onClick={()=> closeSidebar()}
+          >
+
+            <div className="bg-white w-3/4 h-full">
+               <div className="w-full flex justify-end px-2 mt-5">
+                    <button
+                     onClick={()=> closeSidebar()}
+                     className="flex justify-end items-end p-2 bg-slate-50/40 rounded-lg"
+                    >
+                      <IoClose size={25}/>
+                    </button>
+               </div>
+
+               <div className="-mt-10">
+                 <Sidebar/>
+               </div>
+               
+            </div>
+          </div>
+        )}
+
+      </Transition>
+
+    </>
+  )
+}
 
 
 
@@ -62,7 +124,7 @@ function App() {
 
       <Routes>
 
-        <Route element={<Layout />}>    
+        <Route element={<Layout />}>
 
           <Route index path="/" element={<Navigate to="/dashboard" />} />
           <Route path="/dashboard" element={<Dashboard />} />
@@ -76,12 +138,12 @@ function App() {
 
         </Route>
 
-             <Route path="/log-in" element={<Login />} />
+        <Route path="/log-in" element={<Login />} />
 
 
       </Routes>
 
-      <Toaster richColors/>
+      <Toaster richColors />
 
     </main>
   )
